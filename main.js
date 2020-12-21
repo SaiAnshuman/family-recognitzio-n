82d7bcf98fc00a65e0121a4592bf6e7e9bffe204
Sai Playz <71771487+SaiAnshuman@users.noreply.github.com>
2020-12-21 12:35:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,10 +24,35 @@ Webcam.set({
     }
     
     console.log("ml5 version: " + ml5.version);
-    Classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JBpWijEn8/',modelLoaded);
+    Classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oHi2xWOHW/model.json',modelLoaded);
     
     function modelLoaded(){
     
       console.log("Model Has Been Loaded!.");
+    
+    }
+
+    function check(){
+
+      captured_image = document.getElementById("captured_img");
+      Classifier.classify(captured_image,gotResult);
+    
+    }
+    
+    function gotResult(error , results){
+    
+    if (error){
+    
+     console.log(error);
+    
+    }
+    
+    else{
+    
+    console.log(results);
+    document.getElementById("result_obj_name").innerHTML = results[0].label;
+    document.getElementById("result_obj_acc").innerHTML = results[0].confidence.toFixed(3);
+    
+    }
     
     }
